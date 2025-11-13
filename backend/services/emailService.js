7928f16c32ea35ba67ee4cc4e470be2ac53236fd
matrixGen <matrix25102005@gmail.com>
2025-11-13 14:33:54 +0300

    fixes

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -225,7 +225,9 @@ class EmailService {
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD
-      }
+      },
+        logger: true,
+        debug: true,
     });
   }
 
