c28e0e35717a50b88dde0326e6c13fdd8818364d
matrixGen <matrix25102005@gmail.com>
2025-12-31 12:20:04 +0300

    trying fixing chat backend

--- chat-backend/src/server.js
+++ chat-backend/src/server.js
@@ -17,7 +17,7 @@ const io = new Server(httpServer, {
       "http://localhost:5173",
       "http://localhost:3000"
     ],
-    methods: ["GET", "POST"]
+    methods: ["GET", "POST","PUT","PATCH"]
   },
   
   connectionStateRecovery: {
